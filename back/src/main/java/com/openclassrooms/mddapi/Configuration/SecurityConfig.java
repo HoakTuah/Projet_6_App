95d9f5f404ebd1a4416b8b20fe706a460d5ce25f
HoakTuah <balcouille36@gmail.com>
2025-03-25 11:50:22 +0100

    Mise à jour de SecurityConfig pour inclure la configuration CORS

--- back/src/main/java/com/openclassrooms/mddapi/Configuration/SecurityConfig.java
+++ back/src/main/java/com/openclassrooms/mddapi/Configuration/SecurityConfig.java
@@ -44,6 +44,7 @@ public class SecurityConfig {
         @Bean
         public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
                 return http
+                                .cors(cors -> cors.configure(http))
                                 .csrf(csrf -> csrf.disable())
                                 .authorizeHttpRequests(auth -> auth
                                                 .requestMatchers(
